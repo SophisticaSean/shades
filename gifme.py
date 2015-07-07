afc28ff520c3616e58b5f678f20e33cc659adc03
Sean <sensaisean@gmail.com>
2015-07-07 12:47:20 -0400

    revert gifme.py to its better days

--- gifme.py
+++ gifme.py
@@ -1,17 +1,16 @@
 """grabs a gif and posts it in slack"""
-import requests, os, sys, random
+import requests, os, sys
 import resources as rs
 def __main__():
     token = os.getenv('stoken')
     tag = str(sys.argv[1])
     channel = str(sys.argv[2])
 
-    requrl = "http://api.giphy.com/v1/gifs/search?q=" + tag + "&api_key=dc6zaTOxFJmzC"
+    requrl = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tag
 
     try:
         gifjson = requests.get(requrl, timeout=5).json()
-        choice = random.choice(gifjson["data"])
-        gifurl = choice[u"image_url"]
+        gifurl = gifjson[u"data"][u"image_url"]
     except:
         gifurl = 'Sorry bruv, no gifs found with tag(s): ' + tag
 
