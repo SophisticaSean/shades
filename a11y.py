3ee7b7dd864efde6a37a14e202064f559bacf2ac
Sean Lewis <slewis@instructure.com>
2015-07-28 20:55:23 -0600

    more function break out and modularity, priority
    starting to implement priority checks, have base down
    need to build in the actual checks and run them against
    the total a11y problems to get the unprioritzed tix
    then need to alter the sql table and add the applicable
    priority to them

--- a11y.py
+++ a11y.py
@@ -4,21 +4,27 @@ import requests, os, sys, time
 
 def main():
     """main a11y function"""
+    base = "https://{}.atlassian.net/rest/api/2/search?jql=".format(os.getenv('jiradomain'))
+    a11y_base_jql = '(labels%20%3D%20accessibility%20and%20(status%20!%3D%20Closed%20and%20status%20!%3D%20Done)%20and%20project%20!%3D%20"Accessibility%20Testing"%20and%20type%20!%3D%20Epic'
+
+    def construct_jql(jql):
+        """craft our jql with our known beginning and known end"""
+        return base + jql + ')&maxResults=1000'
+
     def get_key(item):
         return item[1]
 
-    def a11y_json():
+    def a11y_json(jql):
         jiracred = (os.getenv('juser') + ':' + os.getenv('jpass')).encode('base64', 'strict')
         headers = {'Authorization': 'Basic ' + jiracred}
 
-        base = "https://{}.atlassian.net/rest/api/2/search?jql=".format(os.getenv('jiradomain'))
-        jql = (base + '(labels%20%3D%20accessibility%20and%20(status%20!%3D%20Closed%20and%20status%20!%3D%20Done)%20and%20project%20!%3D%20"Accessibility%20Testing"%20and%20type%20!%3D%20Epic)&maxResults=1000')
         response = requests.get(jql, headers=headers)
         total = int(response.json()['total'])
         return response.json()
 
     def a11y_list():
-        json = a11y_json()['issues']
+        a11y_list_jql = construct_jql(a11y_base_jql)
+        json = a11y_json(a11y_list_jql)['issues']
         ticket_store = {}
         for item in json:
             if item['fields']['customfield_10007'] != None:
@@ -27,6 +33,11 @@ def main():
                 sprint_count = 0
             ticket_store[item['key']] = {'sprint_count': sprint_count, 'team': item['fields']['customfield_12700']['value']}
         return ticket_store
+
+    def priorized_tickets():
+        print "useless right now"
+        priority_dict = {1: '11163', 2: '11105', 3: '11106'}
+        #need to loop thru this and compare the returned issues to the total issues and list out the unprioritized ones
 
     ticket_store = a11y_list()
     print len(ticket_store)
