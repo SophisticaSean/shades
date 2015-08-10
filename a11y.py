e99b960927b0a50764ec198508278cd12676b75e
Sean Lewis <slewis@instructure.com>
2015-08-10 17:28:11 -0600

    initial a11y commit

---
+++ a11y.py
@@ -0,0 +1,19 @@
+import requests, os, sys, time
+
+def main():
+    """main a11y function"""
+    jiracred = (os.getenv('juser') + ':' + os.getenv('jpass')).encode('base64', 'strict')
+    headers = {'Authorization': 'Basic ' + jiracred}
+
+    base = "https://{}.atlassian.net/rest/api/2/search?jql=".format(os.getenv('jiradomain'))
+    jql = (base + '(labels%20%3D%20accessibility%20and%20(status%20!%3D%20Closed%20and%20status%20!%3D%20Done)%20and%20project%20!%3D%20"Accessibility%20Testing"%20and%20type%20!%3D%20Epic)&maxResults=1000')
+    response = requests.get(jql, headers=headers)
+    total = int(response.json()['total'])
+    for item in response.json()['issues']:
+        if item['fields']['customfield_10007'] != None:
+            sprint_count = len(item['fields']['customfield_10007'])
+        else:
+            sprint_count = 0
+        print item['key'], "sprint count: " + str(sprint_count), item['fields']['customfield_12700']['value'], total
+        total = total - 1
+main()
