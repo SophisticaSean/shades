9e57eeb74940de56ae07140064ecb70a74a07e9a
Sean Lewis <slewis@instructure.com>
2015-08-22 05:40:03 -0600

    more work on a11y.py, unprioritized tix

--- a11y.py
+++ a11y.py
@@ -5,7 +5,7 @@ import requests, os, sys, time
 def main():
     """main a11y function"""
     base = "https://{}.atlassian.net/rest/api/2/search?jql=".format(os.getenv('jiradomain'))
-    a11y_base_jql = '(labels%20%3D%20accessibility%20and%20(status%20!%3D%20Closed%20and%20status%20!%3D%20Done)%20and%20project%20!%3D%20"Accessibility%20Testing"%20and%20type%20!%3D%20Epic'
+    a11y_base_jql = '(labels%20%3D%20accessibility%20and%20(status%20!%3D%20Closed%20and%20status%20!%3D%20Done)%20and%20project%20!%3D%20"Accessibility%20Testing"%20and%20type%20!%3D%20Epic%20'
 
     def construct_jql(jql):
         """craft our jql with our known beginning and known end"""
@@ -18,7 +18,10 @@ def main():
         jiracred = (os.getenv('juser') + ':' + os.getenv('jpass')).encode('base64', 'strict')
         headers = {'Authorization': 'Basic ' + jiracred}
 
-        response = requests.get(jql, headers=headers)
+        try:
+            response = requests.get(jql, headers=headers, timeout=60)
+        except requests.Timeout:
+            print "oops timeout"
         total = int(response.json()['total'])
         return response.json()
 
@@ -34,9 +37,15 @@ def main():
             ticket_store[item['key']] = {'sprint_count': sprint_count, 'team': item['fields']['customfield_12700']['value']}
         return ticket_store
 
-    def priorized_tickets():
-        print "useless right now"
-        priority_dict = {1: '11163', 2: '11105', 3: '11106'}
+    def unprioritized_tickets():
+        priority_dict = {1: '11163', 2: '11105', 3: '11106', 'all': '%20and%20("Epic%20Link"%20%3D%20CNVS-11163%20or%20"epic%20link"%20%3D%20CNVS-11105%20or%20"epic%20link"%20%3D%20CNVS-11106)'}
+        jql = construct_jql(a11y_base_jql + priority_dict['all'])
+        prioritized_tix = a11y_json(jql)['issues']
+        pt_ids = []
+        for item in prioritized_tix:
+            pt_ids.append(item['key'])
+        return list(set(a11y_list().keys()) - set(pt_ids))
+
         #need to loop thru this and compare the returned issues to the total issues and list out the unprioritized ones
 
     ticket_store = a11y_list()
@@ -49,4 +58,5 @@ def main():
     print "The tickets people _want_ to pretend to care about: \r\n"
     for i in temp_arr:
         print "{}, sprint count: {}".format(i[0], i[1])
+    print unprioritized_tickets()
 main()
